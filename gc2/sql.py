a474b2e83eed305e71c9618ddb2ec981ba53f640
Martin Høgh <mh@mapcentia.com>
2024-03-12 14:20:44 +0100

    Split runj in select and transaction

--- gc2/sql.py
+++ gc2/sql.py
@@ -11,11 +11,13 @@ class Sql:
     ):
         self.__gc2 = gc2
         self.__url = f"{gc2.url}/sql"
-        self.data = None
+        self.data_list = None
+        self.data_dict = None
+        self.affected_rows = None
         self.geojson = None
         self.columns = None
 
-    def run(self, sql=None):
+    def select(self, sql=None):
         req = {
             "q": sql,
             "srs": 4326,
@@ -31,8 +33,9 @@ class Sql:
         else:
             self.geojson = resp.text
             res = json.loads(resp.text)
-            table = []
             row = []
+            table_list = []
+            table_dict = []
             # Columns
             for p in res["forGrid"]:
                 if p["type"] != "geometry":
@@ -40,9 +43,28 @@ class Sql:
             self.columns = row
             # Add rows
             for p in res["features"]:
-                row = []
+                _list = []
+                _dict = {}
                 for v in p["properties"]:
-                    row.append(p["properties"][v])
-                table.append(row)
-            self.data = table
+                    value = p["properties"][v];
+                    _list.append(value)
+                    _dict[v] = p["properties"][v]
+                table_list.append(_list)
+                table_dict.append(_dict)
+            self.data_list = table_list
+            self.data_dict = table_dict
+            return
+
+    def transaction(self, sql=None):
+        req = {
+            "q": sql,
+            "base64": None
+        }
+        resp = requests.post(self.__url, headers=self.__gc2.headers, data=json.dumps(req))
+        if resp.status_code != 200:
+            raise Exception(f"Error {resp.status_code}: {resp.text}")
+        else:
+            self.geojson = resp.text
+            res = json.loads(resp.text)
+            self.affected_rows = res['affected_rows']
             return
